c2eedc8732031201fb869db92934ea0ef1432904
rafaelreisramos <rrramos@gmail.com>
2023-05-19 23:49:23 -0300

    Add mobile auth and router

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,5 +1,8 @@
-import { StatusBar } from 'expo-status-bar'
+import { useEffect } from 'react'
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { useAuthRequest, makeRedirectUri } from 'expo-auth-session'
+import * as SecureStore from 'expo-secure-store'
 import { styled } from 'nativewind'
 import {
   useFonts,
@@ -8,13 +11,55 @@ import {
 } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
-import blurBg from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripe.svg'
-import NLWLogo from './src/assets/logo-nlw-spacetime.svg'
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripe.svg'
+import NLWLogo from '../src/assets/logo-nlw-spacetime.svg'
+import { api } from '../src/lib/api'
 
 const StyledStripes = styled(Stripes)
 
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/5310103c3b433fa343c4',
+}
+
 export default function App() {
+  const [, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: '5310103c3b433fa343c4',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlw-spacetime',
+      }),
+    },
+    discovery,
+  )
+
+  async function handleRegister(code: string) {
+    try {
+      const response = await api.post('/register', { code })
+      const { token } = response.data
+      SecureStore.setItemAsync('token', token)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    // console.log(
+    //   makeRedirectUri({
+    //     scheme: 'nlw-spacetime',
+    //   }),
+    // )
+
+    if (response?.type === 'success') {
+      const { code } = response.params
+      handleRegister(code)
+    }
+  }, [response])
+
   const [hasFontsLoaded] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
@@ -49,6 +94,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signInWithGithub()}
         >
           <Text className="font-alt text-sm uppercase text-gray-900">
             COMEÇAR A CADASTRAR
--- server/src/server.ts
+++ server/src/server.ts
@@ -15,6 +15,7 @@ app.register(authRoutes)
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.info('HTTP server running on http://localhost:3333')
