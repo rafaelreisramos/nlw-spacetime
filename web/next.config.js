38590f574c02e7d23dee7e1bec948d8743af0d2b
rafaelreisramos <rrramos@gmail.com>
2023-05-23 16:31:09 -0300

    Add memories page

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com'],
+    // domains: ['avatars.githubusercontent.com', '192.168.15.101'],
+    remotePatterns: [
+      { hostname: 'avatars.githubusercontent.com' },
+      { hostname: 'localhost' },
+      { hostname: '192.168.15.101' },
+    ],
   },
 }
 
